f50112ec52453828ae59f8298e0a03424db7279e
ElMayo <mic.l.meier@gmail.com>
2020-09-27 11:19:07 +0200

    Adding theme ui with default theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'theme-ui'
 
 import App from './components/App/index'
+import theme from './theme'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
